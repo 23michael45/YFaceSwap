c7b9c68c1763ae616ec5b79abda2a2b29e0054b1
23michael45 <michael2345@live.cn>
2019-05-08 11:35:41 +0800

    add compile java to sh

--- Java/src/run.sh
+++ Java/src/run.sh
@@ -1,1 +1,4 @@
- java -Djava.library.path=../../bin com.yfaceswap.Entry
+
+javac com/yfaceswap/FaceSwapLib.java
+javac com/yfaceswap/Entry.java
+java -Djava.library.path=../../bin com.yfaceswap.Entry
